c0220fa8fc94c3d0b6c6197fcf95348f6aaaafcc
Dewapura, Manula (RBI) <DewapuraM@b2b.regn.net>
2020-07-10 19:26:36 +0100

    End of Section 12

---
+++ API/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        private IMediator mediator;
+        protected IMediator Mediator => mediator ?? (mediator = HttpContext.RequestServices.GetService<IMediator>());
+    }
+}
